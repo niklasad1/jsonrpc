6e3b882f763d28fe90df287eafc1d7e90f5756e1
Travis CI User <travis@example.org>
2018-03-15 11:40:51 +0000

    Update documentation

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["jsonrpc_server_utils"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"jsonrpc_server_utils/cors/enum.AccessControlAllowOrigin.html\" title=\"enum jsonrpc_server_utils::cors::AccessControlAllowOrigin\">AccessControlAllowOrigin</a>",];
+implementors["jsonrpc_ws_server"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"jsonrpc_ws_server/struct.Error.html\" title=\"struct jsonrpc_ws_server::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"jsonrpc_ws_server/enum.ErrorKind.html\" title=\"enum jsonrpc_ws_server::ErrorKind\">ErrorKind</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
